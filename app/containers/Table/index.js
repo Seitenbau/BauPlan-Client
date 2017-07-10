943d94b197bd6c644c6ae51877a0ac74c41dd84d
Lukas Ochmann <lukas.ochmann@seitenbau.com>
2017-07-10 06:52:55 +0200

    No-Ticket

--- app/containers/Table/index.js
+++ app/containers/Table/index.js
@@ -4,15 +4,19 @@
  *
  */
 
-import { PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import TableDisplay from 'components/TableDisplay';
 
 import makeSelectTable from './selectors';
 
-export class Table extends TableDisplay { // eslint-disable-line react/prefer-stateless-function
-  // @TODO
+export class Table extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <TableDisplay />
+    );
+  }
 }
 
 Table.propTypes = {
