ac612d730c31c98375b978fee6390b435d67b7ba
Tobias Schmidt <tobias.schmidt82@gmail.com>
2017-12-25 12:44:19 +0100

    Updates

---
+++ static/js/8.d2b91991.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([8],{237:function(t,e,a){"use strict";function i(){var t=arguments.length>0&&void 0!==arguments[0]?arguments[0]:n,e=arguments[1];switch(e.type){case d.a:return t.set("width-height",e.data.width+"-"+e.data.height);default:return t}}Object.defineProperty(e,"__esModule",{value:!0});var h=a(15),d=(a.n(h),a(91)),n=Object(h.fromJS)({"width-height":"0-0"});e.default=i}});
+//# sourceMappingURL=8.d2b91991.chunk.js.map
