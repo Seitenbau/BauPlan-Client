644eac70cc5a5617f3774d7f50e1d327764ed3a9
Lukas Ochmann <lukas.ochmann@seitenbau.com>
2019-02-28 07:49:02 +0100

    fix, default value for urlify

--- src/assets/utils/Urlify.js
+++ src/assets/utils/Urlify.js
@@ -1,8 +1,8 @@
-function toUrl(name) {
+function toUrl(name = '') {
   return name.trim().replace(/ /g, '_');
 }
 
-function fromUrl(url) {
+function fromUrl(url = '') {
   return url.replace(/_/g, ' ');
 }
 export { toUrl, fromUrl };
